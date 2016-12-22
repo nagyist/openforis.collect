dc632f99d4fc187b75e65d32c17a3abdfdfcf44d
fefrain <fefrain@users.noreply.github.com>
2016-12-22 12:07:29 +0100

    Prepare user group management

---
+++ collect-core/src/main/java/org/openforis/collect/model/UserGroup.java
@@ -0,0 +1,103 @@
+package org.openforis.collect.model;
+
+import java.util.Date;
+
+public class UserGroup {
+
+	public enum Visibility {
+		PUBLIC('P'), PRIVATE('N');
+		
+		private char code;
+		
+		Visibility(char code) {
+			this.code = code;
+		}
+		
+		public static Visibility fromCode(char code) {
+			for (Visibility value : values()) {
+				if (value.code == code) {
+					return value;
+				}
+			}
+			throw new IllegalArgumentException("Invalid UserGroup Visibility code: " + code);
+		}
+		
+		public char getCode() {
+			return code;
+		}
+	}
+	
+	private Integer id;
+	private String name;
+	private String label;
+	private String description;
+	private Integer createdById;
+	private User createdBy;
+	private Date creationDate;
+	private Visibility visibility;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getCreatedById() {
+		return createdById;
+	}
+
+	public void setCreatedById(Integer createdById) {
+		this.createdById = createdById;
+	}
+
+	public User getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	
+	public Visibility getVisibility() {
+		return visibility;
+	}
+
+	public void setVisibility(Visibility visibility) {
+		this.visibility = visibility;
+	}
+
+}
